390b8fcea659b4f3c1f511704d1760707bde4992
SwampPear <michaeljosephvaden03@gmail.com>
2024-10-30 18:09:15 -0400

    set up cookies

--- website/static/js/cookies.js
+++ website/static/js/cookies.js
@@ -1,15 +1,54 @@
 const setCookie = (name, value, exp) => {
-    var expires = ""
+    var expires = ''
 
     if ( exp ) {
         var date = new Date()
         date.setTime(date.getTime() + exp)
-        expires = "; expires=" + date + ";"
+        expires = '; expires=' + date + ';'
     }
 
-    document.cookie = name + "=" + (value || "") + expires
+    document.cookie = name + '=' + (value || '') + expires
+}
+
+const getCookie = ( name ) => {
+    let cookies = document.cookie.split( ';' )
+    let cvalue = ''
+
+    cookies.forEach( ( cookie ) => {
+        let cname = cookie.split( '=', 2 )[0].trim()
+
+        if ( name == cname ) {
+            cvalue = cookie.split( '=', 2 )[1].trim()
+        }
+    })
+
+    return cvalue;
 }
 
 document.addEventListener( 'DOMContentLoaded', () => {
+    // DOM
+    const cookiesBanner = document.querySelector( '.cookies__banner' )
+    const acceptButton = document.querySelector( '.cookies__accept' )
+    const rejectButton = document.querySelector( '.cookies__reject' )
 
+    // state
+    let cookies = false;
+
+    const fadeOutBanner = () => {
+        cookiesBanner.classList.add( 'cookies__banner__fade-out' )
+    }
+
+    if ( getCookie( 'gtifc-no-cookie' ) != 'true' && getCookie( 'gtifc-no-cookie' ) != 'false' ) {
+        cookiesBanner.classList.remove( 'cookies__banner__no-display' )
+
+        acceptButton.addEventListener( 'click', () => {
+            setCookie( 'gtifc-no-cookie', 'false', 1000 * 60 * 60 * 24 )
+            fadeOutBanner()
+        })
+    
+        rejectButton.addEventListener( 'click', () => {
+            setCookie( 'gtifc-no-cookie', 'true', 1000 * 60 * 60 * 24 )
+            fadeOutBanner()
+        })
+    }
 })
