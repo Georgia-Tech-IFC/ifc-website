e61710c260647023751fe9b9e70e25ce6c11cafe
SwampPear <michaeljosephvaden03@gmail.com>
2024-10-30 17:08:48 -0400

    working on cookies

--- website/static/js/cookies.js
+++ website/static/js/cookies.js
@@ -1,35 +1,15 @@
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+const setCookie = (name, value, exp) => {
+    var expires = ""
+
+    if ( exp ) {
+        var date = new Date()
+        date.setTime(date.getTime() + exp)
+        expires = "; expires=" + date + ";"
+    }
+
+    document.cookie = name + "=" + (value || "") + expires
 }
 
-const setCooki = (name, value, exdays) => {
-    const date = new Date();
-}
+document.addEventListener( 'DOMContentLoaded', () => {
 
-/*
-const d = new Date();
-let diff = d.getTimezoneOffset();
-console.log(diff)
-
-const date = new Date();
-
-// Get the local date string in the user's timezone
-const localDate = date.toLocaleDateString();
-
-// Get the local date string in a specific timezone
-const newYorkDate = date.toLocaleDateString('en-US', { timeZone: 'America/New_York' });
-const londonDate = date.toLocaleDateString('en-GB', { timeZone: 'Europe/London' });
-
-console.log('Local Date:', localDate);
-console.log('New York Date:', newYorkDate);
-console.log('London Date:', londonDate);
-
-
-document.cookie = "username=John Smith; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/";
-document.cookie = "user=John Smith; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/";
-
-let x = document.cookie;
-*/
+})
