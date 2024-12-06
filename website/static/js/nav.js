0935a23e5f0e31f0f88bc6ca981c9f8ed71c6b56
SwampPear <michaeljosephvaden03@gmail.com>
2024-12-05 21:42:29 -0500

    finished nav mobile styling

--- website/static/js/nav.js
+++ website/static/js/nav.js
@@ -1,6 +1,7 @@
 document.addEventListener( 'DOMContentLoaded', () => {
     const nav = document.querySelector( '.nav' )
     const navLinks = document.querySelectorAll( '.nav__link' )
+    const navMenuButton = document.querySelector( '.nav__menu__button' )
     const contactButton = document.querySelector( '.nav__link__button' )
     const contactForm = document.querySelector( '.contact__container' )
 
@@ -13,6 +14,8 @@ document.addEventListener( 'DOMContentLoaded', () => {
             navWhite = true
             nav.classList.remove( 'nav__fade-out' )
             nav.classList.add( 'nav__fade-in' )
+            navMenuButton.classList.remove( 'nav__button__fade-out' )
+            navMenuButton.classList.add( 'nav__button__fade-in' )
 
             Array.from( navLinks ).forEach( ( el ) => {
                 el.classList.remove( 'nav__link__fade-out' )
@@ -24,6 +27,8 @@ document.addEventListener( 'DOMContentLoaded', () => {
             navWhite = false
             nav.classList.remove( 'nav__fade-in' )
             nav.classList.add( 'nav__fade-out' )
+            navMenuButton.classList.remove( 'nav__button__fade-in' )
+            navMenuButton.classList.add( 'nav__button__fade-out' )
 
             Array.from( navLinks ).forEach( ( el ) => {
                 el.classList.remove( 'nav__link__fade-in' )
