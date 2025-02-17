97f97bfbceb0f91046aa6093b91e64642feb3715
Michael Vaden <michaelvaden.mjv@gmail.com>
2025-02-17 14:32:54 -0500

    changed settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['147.182.216.56']
 # Application definition
 
 INSTALLED_APPS = [
