a6ac57728946c4a6b82fcf45c137446ddfd8d670
Michael Vaden <michaelvaden.mjv@gmail.com>
2025-02-24 18:16:10 -0500

    changed host settings in django settings and turned off debug mode

--- website/website/settings.py
+++ website/website/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['147.182.216.56']
+ALLOWED_HOSTS = ['147.182.216.56', 'www.gatechifc.org', 'gatechifc.org']
 # Application definition
 
 INSTALLED_APPS = [
