a173303fdf7e8365d22c2c5ee5472168d8111eda
Michael Vaden <michaelvaden.mjv@gmail.com>
2025-03-10 12:17:39 -0700

    working on adding an image to ifc philanthropy page

--- website/temp_static/static/js/nav_index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener( 'DOMContentLoaded', () => {
-    const contactButton = document.querySelector( '.nav__link__button' )
-    const contactForm = document.querySelector( '.contact__container' )
-
-    contactButton.addEventListener( 'click', () => {
-        contactForm.scrollIntoView( { behavior: 'smooth' } )
-    })
-})
